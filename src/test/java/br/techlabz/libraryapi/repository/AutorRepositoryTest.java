f3aa963a8d433ffdf853d5444401774af91c2a03
Willian <br.techlabz@proton.me>
2026-01-06 11:07:11 -0300

    Aula 59: Executando testes unitarios de persistencia no banco de dados.

--- src/test/java/br/techlabz/libraryapi/repository/AutorRepositoryTest.java
+++ src/test/java/br/techlabz/libraryapi/repository/AutorRepositoryTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -40,5 +41,36 @@ public class AutorRepositoryTest {
             repository.save(toUpdate);
 
         }
+    }
+
+
+    @Test
+    public void listarTodosTest() {
+        List<Autor> lista = repository.findAll();
+        lista.forEach(System.out::println);
+    }
+
+    @Test
+    public void countTest(){
+        var count = repository.count();
+        System.out.println("Total de autores: " + count);
+    }
+
+    @Test
+    public void deleteByObjectTest(){
+        UUID id = UUID.fromString("d25ef0a8-d0c3-46d9-b893-05551ac3829e");
+        Optional<Autor> recuperado = repository.findById(id);
+        if(recuperado.isPresent()){
+            Autor toDelete = recuperado.get();
+            repository.delete(toDelete);
+            System.out.println("Autor excluído com sucesso " + toDelete.getNome());
+        }
+
+    }
+
+    @Test
+    public void deleteByIdTest(){
+        UUID id = UUID.fromString("d2cdd579-4169-4eb1-9503-4e6f18071534");
+        repository.deleteById(id);
     }
 }
