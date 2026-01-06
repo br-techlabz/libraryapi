caf7c54c51fe13d4cb77120aa61aee8af8dd6f5f
Willian <br.techlabz@proton.me>
2026-01-06 13:16:12 +0000

    Aula 57: Executando uma persistencia no banco de dados

--- src/main/java/br/techlabz/libraryapi/Application.java
+++ src/main/java/br/techlabz/libraryapi/Application.java
@@ -1,13 +1,28 @@
 package br.techlabz.libraryapi;
 
+import br.techlabz.libraryapi.model.Autor;
+import br.techlabz.libraryapi.repository.AutorRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		var context = SpringApplication.run(Application.class, args);
+		AutorRepository repository = context.getBean(AutorRepository.class);
+
+		exemploSalvarRegistro(repository);
 	}
 
+	public static void exemploSalvarRegistro(AutorRepository autorRepository) {
+		Autor autor = new Autor();
+		autor.setNome("Jose Saramago");
+		autor.setNacionalidade("Portuguesa");
+		autor.setDataNascimento(LocalDate.of(1965, 6, 16));
+		var autorSalvo = autorRepository.save(autor);
+		System.out.println("Autor Salvo: " + autorSalvo);
+	}
 }
--- src/main/java/br/techlabz/libraryapi/model/Autor.java
+++ src/main/java/br/techlabz/libraryapi/model/Autor.java
@@ -3,6 +3,7 @@ package br.techlabz.libraryapi.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import org.springframework.cglib.core.Local;
 
 import java.time.LocalDate;
@@ -13,6 +14,7 @@ import java.util.UUID;
 @Table(name = "autor", schema = "public")
 @Getter //gera os metodos getters (Lombok)
 @Setter // gera os metodos setters (Lombok)
+@ToString
 public class Autor {
 
     @Id
